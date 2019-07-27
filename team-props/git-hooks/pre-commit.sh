82c43c9539406e458f2059da2d654be3643fb050
CHINNATHAMBI, KARTHICK <ckarthickit@gmail.com>
2019-07-28 00:03:03 +0530

    Final pre-commit and pre-push hooks added

--- team-props/git-hooks/pre-commit.sh
+++ team-props/git-hooks/pre-commit.sh
@@ -1,17 +1,47 @@
 #!/bin/sh
 
-echo "Running linting analysis... Formatting will be incorporated soon..."
+
+PROGNAME=$(basename $0)
+# use $LINENO as call-site while passing the error-msg
+error_exit()
+{
+	echo "${PROGNAME}: ${1:-"Unknown Error"}" 1>&2
+	exit 1
+}
+
+check_task_result() {
+if [[ "$?" -ne 0 ]] ; then
+    error_exit "$LINENO : issues found. please fix before committing"
+fi
+}
+
+update_staged_files() {
+staged_kotlin_files=`git --no-pager diff --name-status --no-color --cached | awk '$1 != "D" && $2 ~ /\.kts|\.kt/ { print $2}'`
+staged_files=`git --no-pager diff --name-status --no-color --cached | awk '$1 != "D" { print $2}'`
+staged_test_files=`git --no-pager diff --name-status --no-color --cached | awk '$1 != "D" && $2 ~  /\/test\// && $2 ~ /\.kt|\.java/ { print $2}'`
+}
+
+#main() {
+
+update_staged_files
+
+# Do formatting
+echo "running spotless apply and check"
+./gradlew spotlessApply
+check_task_result
+./gradlew spotlessCheck
+check_task_result
+
+# Do linting
+echo "running debug lint"
+./gradlew lintDebug
+check_task_result
 
 # Validate Kotlin code with detekt and KtLint before committing
 #./gradlew detekt ktlintCheck --daemon
-./gradlew lint --daemon
+#./gradlew lint --daemon
 
-status=$?
+# Done
+echo "pre-commit checks passed!"
 
-if [ "$status" = 0 ] ; then
-    echo "Static analysis found no issues. Proceeding with push."
-    exit 0
-else
-    echo 1>&2 "Static analysis found issues you need to fix before pushing."
-    exit 1
-fi
+#}
--- team-props/git-hooks/pre-push.sh
+++ team-props/git-hooks/pre-push.sh
@@ -1,17 +1,39 @@
 #!/bin/sh
 
-echo "Running static analysis..."
+PROGNAME=$(basename $0)
+# use $LINENO as call-site while passing the error-msg
+error_exit()
+{
+	echo "${PROGNAME}: ${1:-"Unknown Error"}" 1>&2
+	exit 1
+}
 
-# Validate Kotlin code with detekt and KtLint before committing
-#./gradlew detekt ktlintCheck --daemon
-./gradlew test --daemon
-
-status=$?
-
-if [ "$status" = 0 ] ; then
-    echo "Static analysis found no issues. Proceeding with push."
-    exit 0
-else
-    echo 1>&2 "Static analysis found issues you need to fix before pushing."
-    exit 1
+# $1 = prefix
+check_task_result() {
+if [[ "$?" -ne 0 ]] ; then
+    error_exit "$LINENO : (line:$1) issues found. please fix before pushing"
 fi
+}
+
+update_staged_files() {
+staged_kotlin_files=`git --no-pager diff --name-status --no-color --cached | awk '$1 != "D" && $2 ~ /\.kts|\.kt/ { print $2}'`
+staged_files=`git --no-pager diff --name-status --no-color --cached | awk '$1 != "D" { print $2}'`
+staged_test_files=`git --no-pager diff --name-status --no-color --cached | awk '$1 != "D" && $2 ~  /\/test\// && $2 ~ /\.kt|\.java/ { print $2}'`
+}
+
+#main() {
+
+# Validate Kotlin code with detekt
+echo "running detekt"
+./gradlew detekt --daemon
+check_task_result $LINENO
+
+# Run unit tests
+echo "running unit tests"
+./gradlew test --daemon
+check_task_result $LINENO
+
+#done
+echo "pre-push checks passed!"
+
+#}
