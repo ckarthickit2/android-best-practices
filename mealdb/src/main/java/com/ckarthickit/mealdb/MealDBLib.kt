46f7dae70098a022d306517941f737c8aaae92cd
CHINNATHAMBI, KARTHICK (kc3503) <karthick.chinnathambi@intl.att.com>
2019-07-27 07:59:35 +0530

    [Add] MealDB library

---
+++ mealdb/src/main/java/com/ckarthickit/mealdb/MealDBLib.kt
@@ -0,0 +1,12 @@
+/* Licensed under Apache-2.0 */
+package com.ckarthickit.mealdb
+
+object MealDBLib {
+  const val NAME = "meal-db"
+  const val version = "1.0.0"
+  fun main() {
+    println("do something")
+    if (true) {
+    }
+  }
+}
