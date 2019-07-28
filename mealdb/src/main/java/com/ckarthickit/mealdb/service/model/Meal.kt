603ba8ff04dd344b7822217caa694d67d71abf04
CHINNATHAMBI, KARTHICK <ckarthickit@gmail.com>
2019-07-28 09:00:22 +0530

    Refactor Meal.kt

--- mealdb/src/main/java/com/ckarthickit/mealdb/service/model/Meal.kt
+++ mealdb/src/main/java/com/ckarthickit/mealdb/service/model/Meal.kt
@@ -4,4 +4,8 @@ import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = true)
-data class Meal(@Json(name = "idMeal") val id: Int, @Json(name = "strMeal") val name: String, @Json(name = "strMealThumb") val thumbnail: String)
+data class Meal(
+  @Json(name = "idMeal") val id: Int,
+  @Json(name = "strMeal") val name: String,
+  @Json(name = "strMealThumb") val thumbnail: String
+)
