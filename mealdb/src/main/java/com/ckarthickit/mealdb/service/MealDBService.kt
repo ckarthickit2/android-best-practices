415ff82f1e97bbe458e0756362c4a30719c8beac
CHINNATHAMBI, KARTHICK <ckarthickit@gmail.com>
2019-07-28 14:17:01 +0530

    [Add] Unit Test cases for MealDBService

--- mealdb/src/main/java/com/ckarthickit/mealdb/service/MealDBService.kt
+++ mealdb/src/main/java/com/ckarthickit/mealdb/service/MealDBService.kt
@@ -1,12 +1,31 @@
 package com.ckarthickit.mealdb.service
 
+import com.ckarthickit.mealdb.service.model.MealsAreaResponse
+import com.ckarthickit.mealdb.service.model.MealsCategoryResponse
+import com.ckarthickit.mealdb.service.model.MealsIngredientsResponse
 import com.squareup.moshi.Moshi
+import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.http.GET
 
 interface MealDBService {
 
   companion object {
+    private var _moshi: Moshi? = null
+
+    val moshi: Moshi
+      get() {
+        if (_moshi == null) {
+          synchronized(MealDBService::class.java) {
+            if (_moshi == null) {
+              _moshi = Moshi.Builder().build()
+            }
+          }
+        }
+        return _moshi as Moshi
+      }
+
     fun create(): MealDBService {
       val moshi = Moshi.Builder().build()
       return Retrofit.Builder()
@@ -17,4 +36,13 @@ interface MealDBService {
         }
     }
   }
+
+  @GET(value = "/list.php?c=list")
+  fun getCategories(): Call<MealsCategoryResponse>
+
+  @GET(value = "/list.php?a=list")
+  fun getAreas(): Call<MealsAreaResponse>
+
+  @GET(value = "/list.php?i=list")
+  fun getAvailableIngredients(): Call<MealsIngredientsResponse>
 }
---
+++ mealdb/src/main/java/com/ckarthickit/mealdb/service/model/MealDBResponse.kt
@@ -0,0 +1,80 @@
+package com.ckarthickit.mealdb.service.model
+
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
+sealed class MealDBResponse<T>(val items: Array<T>) {
+  override fun equals(other: Any?): Boolean {
+    if (this === other) return true
+    if (javaClass != other?.javaClass) return false
+
+    other as MealDBResponse<T>
+
+    if (!items.contentEquals(other.items)) return false
+
+    return true
+  }
+
+  override fun hashCode(): Int {
+    return items.contentHashCode()
+  }
+}
+
+// fun <T> MealDBResponse<T>.equals(other: Any?): Boolean {
+//  if (this === other) return true
+//  if (javaClass != other?.javaClass) return false
+//
+//  other as MealDBResponse<T>
+//
+//  if (!items.contentEquals(other.items)) return false
+//
+//  return true
+// }
+
+@JsonClass(generateAdapter = true)
+data class MealsCategoryResponse(
+  @Json(name = "meals") val categories: Array<Category>
+) : MealDBResponse<Category>(categories) {
+  override fun equals(other: Any?): Boolean {
+    return super.equals(other)
+  }
+
+  override fun hashCode(): Int {
+    return super.hashCode()
+  }
+}
+
+@JsonClass(generateAdapter = true)
+data class MealsAreaResponse(@Json(name = "meals") val areas: Array<Area>) : MealDBResponse<Area>(areas) {
+  override fun equals(other: Any?): Boolean {
+    return super.equals(other)
+  }
+
+  override fun hashCode(): Int {
+    return super.hashCode()
+  }
+}
+
+@JsonClass(generateAdapter = true)
+data class MealsIngredientsResponse(
+  @Json(name = "meals") val ingredients: Array<Ingredient>
+) : MealDBResponse<Ingredient>(ingredients) {
+  override fun equals(other: Any?): Boolean {
+    return super.equals(other)
+  }
+
+  override fun hashCode(): Int {
+    return super.hashCode()
+  }
+}
+
+@JsonClass(generateAdapter = true)
+data class MealsListResponse(@Json(name = "meals") val meals: Array<Meal>) : MealDBResponse<Meal>(meals) {
+  override fun equals(other: Any?): Boolean {
+    return super.equals(other)
+  }
+
+  override fun hashCode(): Int {
+    return super.hashCode()
+  }
+}
