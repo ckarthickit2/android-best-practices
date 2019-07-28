8ad21c932806b4e251abd83e80b1a703ae756f6c
CHINNATHAMBI, KARTHICK <ckarthickit@gmail.com>
2019-07-28 17:37:32 +0530

    [Add] Unit Test for Requests

--- mealdb/src/test/java/com/ckarthickit/mealdb/MealDBServiceTest.kt
+++ mealdb/src/test/java/com/ckarthickit/mealdb/MealDBServiceTest.kt
@@ -5,14 +5,18 @@ import com.squareup.moshi.Moshi
 import kotlinx.coroutines.runBlocking
 import okhttp3.mockwebserver.MockResponse
 import okhttp3.mockwebserver.MockWebServer
+import okhttp3.mockwebserver.RecordedRequest
 import okio.Okio
-import org.junit.jupiter.api.TestInstance
-import org.junit.jupiter.api.TestMethodOrder
-import org.junit.jupiter.api.MethodOrderer
 import org.junit.jupiter.api.AfterAll
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.MethodOrderer
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Order
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
+import org.junit.jupiter.api.TestMethodOrder
 import retrofit2.Retrofit
 import retrofit2.await
 import retrofit2.converter.moshi.MoshiConverterFactory
@@ -43,7 +47,7 @@ class MealDBServiceTest {
   }
 
   @Nested
-  inner class FetchLists {
+  inner class FetchFacetsResponseTest {
     @Order(value = 1)
     @Test
     fun `get available categories`() = runBlocking {
@@ -52,13 +56,9 @@ class MealDBServiceTest {
 
       // when
       val categoriesCall = mealDBService.getCategories()
+      val categoryResponse = categoriesCall.await()
 
       // then
-      // val request: RecordedRequest = mockWebServer.takeRequest(1, TimeUnit.SECONDS)
-      // println("===request is ${request.path}===")
-      // assert(request.path == "/list.php?c=list")
-
-      val categoryResponse = categoriesCall.await()
       assert(categoryResponse.categories.size == 14)
     }
 
@@ -70,48 +70,41 @@ class MealDBServiceTest {
 
       // when
       val areasCall = mealDBService.getAreas()
+      val areasResponse = areasCall.await()
 
       // then
-      // val request = mockWebServer.takeRequest()
-      // assert(request.path == "/list.php?c=list")
-
-      val areasResponse = areasCall.await()
       assert(areasResponse.areas.size == 23)
       assert(areasResponse.areas[0].name == "American")
     }
 
     @Order(value = 3)
     @Test
+    @DisplayName(value = "get available ingredients")
     fun `get available ingredients`() = runBlocking {
       // given
       enqueueResponse("list_ingredients.json")
 
       // when
       val ingredientsCall = mealDBService.getAvailableIngredients()
+      val ingredientsResponse = ingredientsCall.await()
 
       // then
-      // val request = mockWebServer.takeRequest()
-      // assert(request.path == "/list.php?c=list")
-
-      val ingredientsResponse = ingredientsCall.await()
       assert(ingredientsResponse.ingredients.size == 529)
     }
   }
 
   @Nested
-  inner class FilteredLists {
+  inner class FetchFilteredMealResponseTest {
+
     @Order(value = 4)
     @Test
     fun `get meal items filtered by area - canadian`() = runBlocking {
       enqueueResponse("list_meals_area_canadian.json")
       // when
       val mealsListCall = mealDBService.mealsFilteredByArea("Canadian")
+      val mealsListResponse = mealsListCall.await()
 
       // then
-      // val request = mockWebServer.takeRequest()
-      // assert(request.path == "/list.php?c=list")
-
-      val mealsListResponse = mealsListCall.await()
       assert(mealsListResponse.meals.size == 13)
       assert(mealsListResponse.meals[0].name == "BeaverTails")
       assert(mealsListResponse.meals[1].id == 52965)
@@ -124,12 +117,9 @@ class MealDBServiceTest {
       enqueueResponse("list_meals_category_seafood.json")
       // when
       val mealsListCall = mealDBService.mealsFilteredByCategory("Seafood")
+      val mealsListResponse = mealsListCall.await()
 
       // then
-      // val request = mockWebServer.takeRequest()
-      // assert(request.path == "/list.php?c=list")
-
-      val mealsListResponse = mealsListCall.await()
       assert(mealsListResponse.meals.size == 19)
       assert(mealsListResponse.meals[0].name == "Baked salmon with fennel & tomatoes")
       assert(mealsListResponse.meals[1].id == 52819)
@@ -141,13 +131,10 @@ class MealDBServiceTest {
     fun `get meal items filtered by ingredient - chicken`() = runBlocking {
       enqueueResponse("list_meals_ingredient_chicken.json")
       // when
-      val mealsListCall = mealDBService.mealsFilteredByIngredient("Canadian")
+      val mealsListCall = mealDBService.mealsFilteredByIngredient("Smoked Salmon")
+      val mealsListResponse = mealsListCall.await()
 
       // then
-      // val request = mockWebServer.takeRequest()
-      // assert(request.path == "/list.php?c=list")
-
-      val mealsListResponse = mealsListCall.await()
       assert(mealsListResponse.meals.size == 10)
       assert(mealsListResponse.meals[0].name == "Brown Stew Chicken")
       assert(mealsListResponse.meals[1].id == 52846)
@@ -155,7 +142,65 @@ class MealDBServiceTest {
     }
   }
 
-  private fun enqueueResponse(fileName: String, headers: Map<String, String> = emptyMap()) {
+  @Nested
+  inner class RequestTest {
+
+    private lateinit var mockServer: MockWebServer
+    private lateinit var mealsService: MealDBService
+
+    @BeforeEach
+    private fun beforeEach() {
+      mockServer = MockWebServer()
+      val moshi = Moshi.Builder().build()
+      mealsService =
+        Retrofit.Builder()
+          .baseUrl(mockServer.url("/"))
+          .addConverterFactory(MoshiConverterFactory.create(moshi))
+          .build().let {
+            it.create(MealDBService::class.java)
+          }
+    }
+
+    @AfterEach
+    fun afterEach() {
+      mockServer.shutdown()
+    }
+
+    @Order(value = 7)
+    @Test
+    fun `verify filterByArea request format`() = runBlocking {
+      enqueueResponse("list_meals_area_canadian.json", mockServer = mockServer)
+      val mealsListCall = mealsService.mealsFilteredByArea("Canadian")
+      mealsListCall.await()
+      val recordedRequestTest: RecordedRequest = mockServer.takeRequest()
+      assert(recordedRequestTest.path == "/api/json/v1/1/filter.php?&a=Canadian")
+      assert(recordedRequestTest.method == "GET")
+    }
+
+    @Order(value = 8)
+    @Test
+    fun `verify filterByCategory request format`() = runBlocking {
+      enqueueResponse("list_meals_category_seafood.json", mockServer = mockServer)
+      val mealsListCall = mealsService.mealsFilteredByCategory("Seafood")
+      mealsListCall.await()
+      val recordedRequestTest: RecordedRequest = mockServer.takeRequest()
+      assert(recordedRequestTest.path == "/api/json/v1/1/filter.php?&c=Seafood")
+      assert(recordedRequestTest.method == "GET")
+    }
+
+    @Order(value = 9)
+    @Test
+    fun `verify filterByIngredient request format`() = runBlocking {
+      enqueueResponse("list_meals_ingredient_chicken.json", mockServer = mockServer)
+      val mealsListCall = mealsService.mealsFilteredByIngredient("Smoked Salmon")
+      mealsListCall.await()
+      val recordedRequestTest: RecordedRequest = mockServer.takeRequest()
+      assert(recordedRequestTest.path == "/api/json/v1/1/filter.php?&i=Smoked%20Salmon")
+      assert(recordedRequestTest.method == "GET")
+    }
+  }
+
+  private fun enqueueResponse(fileName: String, headers: Map<String, String> = emptyMap(), mockServer: MockWebServer = mockWebServer) {
     val inputStream = javaClass.classLoader
       .getResourceAsStream("mealdb-api-response/$fileName")
     val source = Okio.buffer(Okio.source(inputStream))
@@ -163,7 +208,7 @@ class MealDBServiceTest {
     for ((key, value) in headers) {
       mockResponse.addHeader(key, value)
     }
-    mockWebServer.enqueue(
+    mockServer.enqueue(
       mockResponse
         .setBody(source.readString(Charsets.UTF_8))
     )
