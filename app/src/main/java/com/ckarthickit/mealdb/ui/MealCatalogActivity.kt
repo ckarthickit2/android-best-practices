cf3003f05dec6b6d57243cb21f5771f2f38b9b43
CHINNATHAMBI, KARTHICK <ckarthickit@gmail.com>
2019-07-29 10:39:59 +0530

    [Fix] UI alignment

--- app/src/main/java/com/ckarthickit/mealdb/ui/MealCatalogActivity.kt
+++ app/src/main/java/com/ckarthickit/mealdb/ui/MealCatalogActivity.kt
@@ -16,7 +16,7 @@ class MealCatalogActivity : AppCompatActivity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_main)
+    setContentView(R.layout.activity_catalog)
     setSupportActionBar(findViewById(R.id.toolbar))
     initViews()
     setupViewModel()
