f8c073bde0f63901f1a4b37ff0ff9f512e62344d
Karthick Chinnathambi <karthick.chinnathambi@byjus.com>
2020-11-28 22:19:56 +0530

    generate detekt error report

--- mealdb/src/main/java/com/ckarthickit/mealdb/service/model/MealDBResponse.kt
+++ mealdb/src/main/java/com/ckarthickit/mealdb/service/model/MealDBResponse.kt
@@ -6,7 +6,7 @@ import com.squareup.moshi.JsonClass
 internal sealed class MealDBResponse<T>(val items: Array<T>) {
   override fun equals(other: Any?): Boolean {
     if (this === other) return true
-    if (javaClass != other.javaClass) return false
+    if (javaClass != other!!.javaClass) return false
 
     other as MealDBResponse<T>
 
@@ -76,5 +76,9 @@ internal data class MealsListResponse(@Json(name = "meals") val meals: Array<Mea
 
   override fun hashCode(): Int {
     return super.hashCode()
+  }
+
+  fun emptyFunc() {
+
   }
 }
